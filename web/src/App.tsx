13e0816762b277a5ac45e0db900c48a7aaf97fd3
Richard W. L. Brehmer <richard@bnu.com.br>
2021-01-06 16:33:35 -0300

    initial setup

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// It is not necessary anymore
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
